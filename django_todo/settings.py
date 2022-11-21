31bdbe721d327c19811285d33a78b8a8524e88cd
Madebybrown <madebybrown@gmail.com>
2022-11-21 08:57:30 +0000

    Update allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['git.heroku.com/mrbrown-django-todo-app.git']
+ALLOWED_HOSTS = ['mrbrown-django-todo-app.herokuapp.com']
 
 
 # Application definition
